ad714fb51d38ea2f898f2f969a8d289874510403
Maria Victoria <viictoria931@gmail.com>
2022-11-04 12:43:30 -0300

    aa

--- App.js
+++ App.js
@@ -4,82 +4,20 @@ import { useFonts } from 'expo-font';
 import Carreira from './pages/Carreira';
 import Habilidades from './pages/Habilidades';
 import Home from './pages/Home';
-const theme = extendTheme({
-  fontConfig: {
-    Quicksand: {
-      300: {
-        normal: "Quicksand-Light",
-      },
-      400: {
-        normal: "Quicksand-Regular",
-      },
-      500: {
-        normal: "Quicksand-Medium",
-      },
-      600: {
-        normal: "Quicksand-SemiBold",
-      },
-      700: {
-        normal: "Quicksand-Bold",
-      },
-    },
-   
-  },
-  // Make sure values below matches any of the keys in `fontConfig`
-  fonts: {
-    heading: "Quicksand",
-    body: "RobotoSlab",
-    mono: "Quicksand",
-  },
-  colors: {
-      primary: '#9098E0',
-      secondary: '#B2DECD',
-      tertiary: '#273444',
-      active: '#918ECB',
-      black: '#1F2D3D',
-      white: '#D3DCE6',
-      whiteSnow: '#D3DCE6',
-  },
-  Text: {
-      defaultProps: {
-        size: 'sm',
-        color: 'primary',
-        fontFamily: 'Quicksand'
-      },
-  },
-  Input: {
-      defaultProps: {
-        size: 'sm',
-        borderColor: 'primary',
-        color: 'primary',
-        fontFamily: 'Quicksand',        
-      },
-  }
-});
+
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Quicksand': require('./assets/fonts/Quicksand-VariableFont_wght.ttf'),
-  
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  } 
-
+export default function MyStack() {
   return (
-  
-      <NavigationContainer theme={theme}>
-        <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="main" component={Home} options={{ headerShown: false }} />
+    <NavigationContainer>
+    <Stack.Navigator>
+       <Stack.Screen name="main" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Carreira" component={Carreira} options={{ headerShown: false }} />
       <Stack.Screen name="Habilidades" component={Habilidades} options={{ headerShown: false }} />
 
-    
-        </Stack.Navigator>
-      </NavigationContainer>
-  
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
